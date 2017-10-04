6a3a2ce227224bb337ff966b9d4f631a12762832
a-kanaan <32527242+a-kanaan@users.noreply.github.com>
2017-10-05 07:11:32 +0800

    Add files via upload

---
+++ RunningConsoleOnDockerSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace RunningConsoleOnDockerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            Console.WriteLine($"a simple generated word: " +
+                $"{new String(Enumerable.Range(1, 10).Select(_ => (char)r.Next(60, 120)).ToArray())}");
+        }
+    }
+}
